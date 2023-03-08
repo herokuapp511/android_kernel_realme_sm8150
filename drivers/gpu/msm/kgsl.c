97d3c89bb0534cf1f4803e6c61d725e44036f0a0
John Galt <johngaltfirstrun@gmail.com>
2023-03-07 22:40:44 -0500

    Revert "Revert "msm: kgsl: Place worker thread on SCHED_RR & WQ_HIGHPRI""
    This reverts commit b062bf4a55b8f970b69790f93b14e0244e16dc4c.

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -5289,7 +5289,7 @@ static int __init kgsl_core_init(void)
 	}
 
 	kgsl_driver.mem_workqueue = alloc_workqueue("kgsl-mementry",
-		WQ_MEM_RECLAIM, 0);
+		WQ_HIGHPRI | WQ_MEM_RECLAIM, 0);
 
 	kthread_init_worker(&kgsl_driver.worker);
 	
